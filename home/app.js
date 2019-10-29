b93fff75aa32b38f31c1999fa3b0f70a3bf33a6d
Caleb Pendergraft <cackmed@gmail.com>
2019-10-29 16:30:41 -0700

    added first level of image filtering

--- home/app.js
+++ home/app.js
@@ -2,6 +2,36 @@ import renderImages from './render-img.js';
 import images from '../data/images.js';
 import htmlToDOM from '../util/html-to-DOM.js';
 const dev = document.getElementById('grid');
+const dropDown = document.querySelector('.menu');
+
+dropDown.addEventListener('change', () => {
+    const filterString = dropDown.value;
+    let filteredImages = null;
+
+    if (!filterString) {
+        filteredImages = images;
+    }
+    else {
+        filteredImages = images.filter(images => {
+            if (images.horns === filterString) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+    
+    const setImages = filteredImages.map(images => {
+        return {
+            title: images.title,
+            url: images.url,
+            description: images.description,
+            horns: images.horns,
+            keyword: images.keyword,
+        };
+    });
+    render(setImages);
+});
 
 
 const mapArr = ({
