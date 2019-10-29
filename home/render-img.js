e526cdba738d3c5c7c5d0e717ddf34611861fdb3
Caleb Pendergraft <cackmed@gmail.com>
2019-10-29 12:52:15 -0700

    got static html test to pass

---
+++ home/render-img.js
@@ -0,0 +1,10 @@
+export default function renderImage(images) {
+    const html = /*html*/`
+            <div class="image-container">
+            <img src="${images.url}" alt="${images.title} image">
+                <h3>${images.title}</h3>
+                <p>Num of Horns:${images.horns}</p>
+            </div>
+    `;
+    return html;
+}
---
+++ test/render-items.js
@@ -0,0 +1,21 @@
+import renderImage from '../home/render-img.js';
+const test = QUnit.test;
+test('testing that renderImage function that renders html', function(assert) {
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const images = { url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+        title: 'UniWhal',
+        description: 'A unicorn and a narwhal nuzzling their horns',
+        keyword: 'narwhal',
+        horns: 1 };
+    
+    const expected = `<div class="image-container">
+    <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="UniWhal image">
+    <h3>UniWhal</h3>
+    <p>Num of Horns:1</p>
+    </div>`; 
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.htmlEqual(renderImage(images), expected);
+});
